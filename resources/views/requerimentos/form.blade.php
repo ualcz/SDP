7051b94c650e9ee8b6587987719cf2b57878882e
caiosouza-cs <souza.caio.cs73@gmail.com>
2026-09-10 11:50:11 -0300

    style: Melhora a navegação entre setores e simplifica visualição das informações dos alunos

--- resources/views/requerimentos/form.blade.php
+++ resources/views/requerimentos/form.blade.php
@@ -11,12 +11,12 @@
     <!-- Navegação entre modelos / setores -->
     <div class="setores-nav">
         <strong>Setor:</strong>
-        @foreach($modelos as $chave => $mod)
-            <a href="{{ route('requerimentos.aluno.novo', ['modelo' => $chave]) }}"
-               class="{{ ($modeloChave ?? '') === $chave ? 'active' : '' }}">
-                [{{ $mod['setor_sigla'] ?? strtoupper($chave) }}]
-            </a>
-        @endforeach
+            @foreach($modelos as $chave => $mod)
+                <a href="{{ route('requerimentos.aluno.novo', ['modelo' => $chave]) }}"
+                   class="{{ ($modeloChave ?? '') === $chave ? 'active' : '' }} btn-nav">
+                    {{ $mod['setor_sigla'] ?? strtoupper($chave) }}
+                </a>
+            @endforeach
     </div>
 
     <!-- Título do Requerimento -->
@@ -62,42 +62,47 @@
                 </div>
             </div>
 
-            <div class="form-linha">
-                <div class="campo">
-                    <label>E-mail Pessoal (editável):</label>
-                    <input type="email" name="email_pessoal" value="{{ old('email_pessoal', auth()->user()->email_pessoal ?? '') }}" placeholder="seu.email@exemplo.com">
+            <div class="info-aluno">
+                <div class="form-linha">
+                    <div class="campo">
+                        <label>E-mail Pessoal (editável):</label>
+                        <input type="email" name="email_pessoal" value="{{ old('email_pessoal', auth()->user()->email_pessoal ?? '') }}" placeholder="seu.email@exemplo.com">
+                    </div>
+                    <div class="campo">
+                        <label>Telefone / WhatsApp (editável):</label>
+                        <input type="text" name="telefone" value="{{ old('telefone', auth()->user()->telefone ?? '') }}" placeholder="(XX) XXXXX-XXXX">
+                    </div>
                 </div>
-                <div class="campo">
-                    <label>Telefone / WhatsApp (editável):</label>
-                    <input type="text" name="telefone" value="{{ old('telefone', auth()->user()->telefone ?? '') }}" placeholder="(XX) XXXXX-XXXX">
-                </div>
-            </div>
 
-            <div class="form-linha">
-                <div class="campo" style="flex: 2; min-width: 240px;">
-                    <label>Rua e Número (editável):</label>
-                    <input type="text" name="rua" value="{{ old('rua', auth()->user()->endereco?->rua ?? '') }}" placeholder="Ex: Rua Antonio Francisco, 60">
+                <div class="form-linha">
+                    <div class="campo" style="flex: 2; min-width: 240px;">
+                        <label>Rua e Número (editável):</label>
+                        <input type="text" name="rua" value="{{ old('rua', auth()->user()->endereco?->rua ?? '') }}" placeholder="Ex: Rua Antonio Francisco, 60">
+                    </div>
+                    <div class="campo" style="flex: 1.5; min-width: 150px;">
+                        <label>Bairro:</label>
+                        <input type="text" name="bairro" value="{{ old('bairro', auth()->user()->endereco?->bairro ?? '') }}" placeholder="Bairro">
+                    </div>
                 </div>
-                <div class="campo" style="flex: 1.5; min-width: 150px;">
-                    <label>Bairro:</label>
-                    <input type="text" name="bairro" value="{{ old('bairro', auth()->user()->endereco?->bairro ?? '') }}" placeholder="Bairro">
-                </div>
-            </div>
 
-            <div class="form-linha">
-                <div class="campo" style="flex: 2; min-width: 180px;">
-                    <label>Cidade:</label>
-                    <input type="text" name="cidade" value="{{ old('cidade', auth()->user()->endereco?->cidade ?? '') }}" placeholder="Cidade">
-                </div>
-                <div class="campo" style="flex: 0.8; min-width: 80px;">
-                    <label>Estado (UF):</label>
-                    <input type="text" name="estado" maxlength="2" value="{{ old('estado', auth()->user()->endereco?->estado ?? '') }}" placeholder="BA" style="text-transform: uppercase;">
-                </div>
-                <div class="campo" style="flex: 1.2; min-width: 130px;">
-                    <label>CEP:</label>
-                    <input type="text" name="cep" value="{{ old('cep', auth()->user()->endereco?->cep ?? '') }}" placeholder="00000-000">
+                <div class="form-linha">
+                    <div class="campo" style="flex: 2; min-width: 180px;">
+                        <label>Cidade:</label>
+                        <input type="text" name="cidade" value="{{ old('cidade', auth()->user()->endereco?->cidade ?? '') }}" placeholder="Cidade">
+                    </div>
+                    <div class="campo" style="flex: 0.8; min-width: 80px;">
+                        <label>Estado (UF):</label>
+                        <input type="text" name="estado" maxlength="2" value="{{ old('estado', auth()->user()->endereco?->estado ?? '') }}" placeholder="BA" style="text-transform: uppercase;">
+                    </div>
+                    <div class="campo" style="flex: 1.2; min-width: 130px;">
+                        <label>CEP:</label>
+                        <input type="text" name="cep" value="{{ old('cep', auth()->user()->endereco?->cep ?? '') }}" placeholder="00000-000">
+                    </div>
                 </div>
             </div>
+            <button type="button" id="ver-info-aluno" onclick="verInfoAluno()" class="inline-flex justify-center items-center w-10 h-10">
+                <img src="{{ asset('img/icons/chevron-down.svg') }}" class="icon">
+            </button>
         </fieldset>
 
         <!-- 2. Objeto do Requerimento -->
@@ -150,6 +155,6 @@
             Enviar Requerimento
         </button>
     </form>
-
+<script src="{{ asset('js/form.js') }}"></script>
 </div>
 @endsection
