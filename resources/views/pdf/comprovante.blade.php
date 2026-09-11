4f69e37aee5bd823ded8e05a89c803da0360148f
LarissaRocha08 <larysba08@gmail.com>
2026-09-11 15:45:52 -0300

    style: melhora estilo do comprovante

--- resources/views/pdf/comprovante.blade.php
+++ resources/views/pdf/comprovante.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Requerimento - {{ $nomeRequerente->nome }}</title>
+    <style>
+        {!! file_get_contents(public_path('css/comprovante-pdf.css')) !!}
+    </style>
 </head>
 <body>
     {{-- ====== CABEÇALHO INSTITUCIONAL ====== --}}
-    <table style="width: 100%;">
+    <table style="width: 100%;" class="cabecalho">
         <tr>
             <td>
                 <img src="{{ public_path('img/logoVertical.png') }}" alt="Logo IFBA" style="width: 60px;">
@@ -19,33 +22,31 @@
         </tr>
     </table>
 
+    <p class="titulo"><strong>COMPROVANTE DE PROTOCOLO DE REQUERIMENTO - {{ date('Y') }}</strong></p>
+    <div class="protocolo">
+        <p><strong>Data da Solicitação:</strong> 
+            {{ $dataSolicitacao ? $dataSolicitacao->format('d/m/Y H:i') : date('d/m/Y') }}
+        </p>
+        <p><strong>Número do protocolo:</strong> {{ $numeroProtocolo }}</p>
+    <div>
     <hr>
 
-    <p><strong>COMPROVANTE DE PROTOCOLO DE REQUERIMENTO - {{ date('Y') }}</strong></p>
-    <p><strong>Data da Solicitação:</strong> 
-        {{ $dataSolicitacao ? $dataSolicitacao->format('d/m/Y H:i') : date('d/m/Y') }}
-    </p>
-    <p><strong>Número do protocolo:</strong> {{ $numeroProtocolo }}</p>
-
-    <hr>
-
-    <h4>IDENTIFICAÇÃO DO REQUERENTE</h4>
+    <h4>INFORMAÇÕES ADICIONAIS</h4>
     <table border="1" cellpadding="5" cellspacing="0">
         <tr>
-            <td><strong>Nome do requerente:</strong></td>
+            <th><strong>Nome do requerente:</strong></th>
             <td>{{ $nomeRequerente->nome }}</td>
         </tr>
         <tr>
-            <td><strong>Número da turma:</strong></td>
+            <th><strong>Número da turma:</strong></th>
             <td>{{ $nomeRequerente->turma_codigo ?? 'Não informado' }}</td>
         </tr>
         <tr>
-            <td><strong>Objeto do requerimento:</strong></td>
+            <th><strong>Objeto do requerimento:</strong></th>
             <td>{{ $objeto ?? 'Não informado' }}</td>
         </tr>
     </table>
     <hr>
     <p><small>Documento gerado automaticamente pelo SDP - IFBA Campus Seabra | {{ date('d/m/Y H:i:s') }}</small></p>
-
 </body>
 </html>
